26da7b6de725c7794a85f3bf896461e5772a469f
jinwoo park <swat018@gmail.com>
2022-01-08 07:42:18 +0900

    스프링 부트 Actuator 1부: 소개

--- src/main/java/com/example/springbootactuatordemo/Application.java
+++ src/main/java/com/example/springbootactuatordemo/Application.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
     
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
